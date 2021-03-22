f936cc637894466378224a895987dce9ba407442
vrudas <vrudas4work@gmail.com>
2021-03-22 23:31:24 +0200

    Implemented score system

--- Level/Level.gd
+++ Level/Level.gd
@@ -1,14 +1,21 @@
 extends Node
 
 onready var ball: KinematicBody2D = $Ball
+onready var playerScoreLabel: Label = $PlayerScore
+onready var opponentScoreLabel: Label = $OpponentScore
+
+var playerScore: int = 0
+var opponentScore: int = 0
 
 
 func _on_WallLeft_body_entered(body: Node) -> void:
     reset_ball_position()
+    opponentScore += 1
 
 
 func _on_WallRight_body_entered(body: Node) -> void:
     reset_ball_position()
+    playerScore += 1
 
 
 func reset_ball_position() -> void:
@@ -17,3 +24,8 @@ func reset_ball_position() -> void:
         resolution.x / 2,
         resolution.y / 2
     )
+
+
+func _process(delta: float) -> void:
+    playerScoreLabel.text = str(playerScore)
+    opponentScoreLabel.text = str(opponentScore)
