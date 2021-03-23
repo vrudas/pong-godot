902d51b8895564c316a23e22c73a8d8090558852
vrudas <vrudas4work@gmail.com>
2021-03-23 22:37:59 +0200

    Add restart ball functionality

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -6,9 +6,7 @@ var velocity: Vector2 = Vector2.ZERO
 
 
 func _ready() -> void:
-    randomize()
-    velocity.x = [-1, 1][randi() % 2]
-    velocity.y = [-0.8, 0.8][randi() % 2]
+    randomize_initial_movemennt()
 
 
 func _physics_process(delta: float) -> void:
@@ -20,3 +18,14 @@ func _physics_process(delta: float) -> void:
 
 func stop_ball() -> void:
     speed = 0
+
+
+func restart_ball() -> void:
+    speed = 600
+    randomize_initial_movemennt()
+
+
+func randomize_initial_movemennt() -> void:
+    randomize()
+    velocity.x = [-1, 1][randi() % 2]
+    velocity.y = [-0.8, 0.8][randi() % 2]
